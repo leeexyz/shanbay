fc9e882387e7864ec561219e95769fbad0bec746
ColdinLee <coldin.lee@gmail.com>
2016-04-25 23:06:04 +0800

    Support to get page of news.

--- shanbay.py
+++ shanbay.py
@@ -22,7 +22,6 @@ print 'csrftoken', csrftoken
 
 login_data = {'username': username,
               'password': password,
-              #'userid': userid,
               'csrfmiddlewaretoken': csrftoken,}
 
 r = s.post(login_url, data=login_data, headers=headers)
@@ -33,41 +32,50 @@ auth_token = s.cookies['auth_token']
 sessionid = s.cookies['sessionid']
 userid = s.cookies['userid']
 
-response = s.get(news_url, data=login_data, headers=headers)
-json_r = response.json()
 
+print '--------------'
 i = 0
+j = 0
+while i <= 2:
+    j += 1
+    get_news_data = {'page': j,
+                     'ipp': 15,
+                    }
+    response = s.get(news_url + '?page=' + str(j) + '&ipp=15', headers=headers)
+    json_r = response.json()
+    
+    if json_r['status_code'] == 0:
+    
+       print "Get news of page", j 
 
-if json_r['status_code'] == 0:
-
-   news = json_r['data']['articles']
-
-   for new in news:
-       print '--------------'
-       if i >= 2:
-           checkin_r = s.post(checkin_url, headers = headers) 
-           checkin_json = checkin_r.json() 
-           print "return mgs is :", checkin_json['data']
-           break
-       if new['finished']:
-           print "Readed..."
-           continue
-
-       newid = str(new['id'])
-
-       min_used_seconds = str(new['min_used_seconds'] + random.randint(60,120))
-
-       read_data = {'operation': 'finish',
-                    'used_time': min_used_seconds,}
-       read_url = 'http://www.shanbay.com/api/v1/read/article/user/' + newid + '/'
-
-       print "reading url is : "
-       print read_url
-       read_r = s.put(read_url, read_data, headers=headers)
-       print read_r, read_r.text
-       if read_r.json()['status_code'] == 0:
-           i += 1
-           print 'Finnish reading.'
-           time.sleep(3)
-       else:
-           print 'Reading failed.'
+       news = json_r['data']['articles']
+       print "Get", len(news), "news."
+    
+       for new in news:
+           if i > 2:
+               checkin_r = s.post(checkin_url, headers = headers) 
+               checkin_json = checkin_r.json() 
+               print "return mgs is :", checkin_json['data']
+               break
+           if new['finished']:
+               print "Readed..."
+               continue
+    
+           newid = str(new['id'])
+    
+           min_used_seconds = str(new['min_used_seconds'] + random.randint(60,120))
+    
+           read_data = {'operation': 'finish',
+                        'used_time': min_used_seconds,}
+           read_url = 'http://www.shanbay.com/api/v1/read/article/user/' + newid + '/'
+    
+           print "reading url is : "
+           print read_url
+           read_r = s.put(read_url, read_data, headers=headers)
+           print read_r, read_r.text
+           if read_r.json()['status_code'] == 0:
+               i += 1
+               print 'Finnish reading.'
+               time.sleep(3)
+           else:
+               print 'Reading failed.'
